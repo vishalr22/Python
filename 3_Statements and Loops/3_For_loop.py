44e37b3f08170f9ca275c96a681cfdcd7aa922ea
Vishal Rawat <vishal22rawat@gmail.com>
2023-11-21 00:02:54 +0530

    For loop with else

--- 3_Statements and Loops/3_For_loop.py
+++ 3_Statements and Loops/3_For_loop.py
@@ -17,3 +17,23 @@ for x in colors:
 
 for i in range(1, 15, 2):
     print(i)
+
+
+'''
+For loop with else
+Python allows the else keyword to be used with the for loops. The else block appears after the body of the loop. 
+The statements in the else block will be executed after all iterations are completed.
+The program exits the loop only after the else block is executed.
+
+Syntax: 
+for counter in sequence:
+    #Statements inside for loop block
+else:
+    #Statements inside else block
+'''
+
+for x in range(5):
+    print("iteration no {} in for loop".format(x+1))
+else:
+    print("else block in loop")
+print("Out of loop")
