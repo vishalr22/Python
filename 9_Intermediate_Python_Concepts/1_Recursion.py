196f4e1a044357aceba1fd3466a444d97eb78c9d
Vishal Rawat <vishal22rawat@gmail.com>
2023-11-30 14:52:46 +0530

    Recursion

--- 9_Intermediate_Python_Concepts/1_Recursion.py
+++ 9_Intermediate_Python_Concepts/1_Recursion.py
@@ -3,6 +3,8 @@ Recursion is the process of defining something in terms of itself.
 Python Recursive Function is a function where it call itself.
 '''
 
+print("!---------factorial----------!")
+
 
 def factorial(num):
     if (num == 1 or num == 0):
@@ -14,3 +16,15 @@ def factorial(num):
 num = 7
 print("Number: ", num)
 print("Factorial: ", factorial(num))
+
+
+print("!---------fibonacci----------!")
+
+
+def fibonacci(n):
+    return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+
+
+n = int(input("Enter the number: "))
+for i in range(n):
+    print(fibonacci(i), end=" ")
